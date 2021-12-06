3a15ce35228a7c5c188e169b664573b7e9ec944d
Shane Ferrell <shaneferrellwv@gmail.com>
2021-12-05 23:32:13 -0500

    some parsing

--- main.cpp
+++ main.cpp
@@ -2,14 +2,10 @@
 #include <fstream>
 #include <string>
 #include <set>
-#include "graph.h"
-#include "person.h"
-
+#include <algorithm>
 using namespace std;
-using std::fstream;
 
-//============ file in/out ============
-void input()
+int main() 
 {
     ifstream file("data.txt");
     const int NUMBER_OF_FILMS = 146725;
@@ -17,30 +13,52 @@ void input()
     if (file.is_open())
     {
         string line; // "line" is the whole line from the data file, "line" contains all data for a single film
+        // separate the string into respective parts
+        string title;
+        string cast;
+        string directors;
+        string producers;
 
         // while there is a line to be gotten
-        while (getline(file, line))
+        for (int i = 0; i < 146727; i++)
         {
-            // separate the string into respective parts
-            string title;
-            string cast;
-            string directors;
-            string producers;
-            string companies; // not used by program
-            string year;
+            getline(file, line);
 
-            title = line.substr(10, line.find("cast") - 13);
+            int hasTitle = line.find("title");
+            int hasCast = line.find("cast");
+            int hasDirectors = line.find("directors");;
+            int hasProducers = line.find("producers");
 
-            cast = line.substr(line.find("cast") + 6, line.find("directors") - 29);
-            
-            cout << title << endl;
-            cout << cast << endl;
+            if (hasTitle > 0 && hasCast > 0 && hasDirectors > 0 && hasProducers > 0)
+            {
+
+                title = line.substr(10, line.find("cast") - 13);
+                line = line.substr(line.find("cast") - 13);
+
+                cast = line.substr(19, line.find("directors") - 21);
+                if (cast != "[]}" && cast != "[]") {
+                    line = line.substr(line.find("directors") + 13);
+                    for (int j = 0; j < (count(cast.begin(), cast.end(), ']') - 1) / 2; j++)
+                    {
+                        string actorName = cast.substr(2, cast.find(']') - 2);
+                        cast = cast.substr(cast.find(']') + 3);
+                        //if: actor vertex does not exist
+                            //create new actor vertex
+                        //else:
+                            //add current film to actor vertex
+                        cout << actorName << endl;
+                    }
+
+
+                    directors = line.substr(0, line.find("producers") - 2);
+                    line = line.substr(line.find("producers") + 11);
+
+                    producers = line.substr(0, line.find("companies") - 2);
+                }
+            }
         }
     }
-}
 
 
-int main() {
-    input();
     return 0;
 }
